4e8b5304135d2dbb877b84e939859e0482fc0205
ishwar6 <ishawarjangid66@gmail.com>
2020-12-01 23:40:34 +0530

    Yfinance

--- apps.py
+++ apps.py
@@ -1,1 +1,25 @@
-import numpy as np
+
+import pandas as pd
+import yfinance as yf
+
+
+df_yahoo = yf.download('AAPL', start='2000-01-01',
+                       end='2010-01-01', progress=False)
+
+msft = yf.Ticker("MSFT")
+print(msft)
+
+# get stock info
+print(msft.info)
+
+# get historical market data
+hist = msft.history(period="max")
+
+# show actions (dividends, splits)
+msft.actions
+
+# show dividends
+msft.dividends
+
+# show splits
+msft.splits
