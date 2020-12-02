1c73dd292c52d4a7baa020d7e71d26aa948eadeb
ishwar6 <ishawarjangid66@gmail.com>
2020-12-02 20:09:53 +0530

    DF

--- apps.py
+++ apps.py
@@ -1,25 +1,27 @@
 
 import pandas as pd
 import yfinance as yf
+import numpy as np
 
 
-df_yahoo = yf.download('AAPL', start='2000-01-01',
-                       end='2010-01-01', progress=False)
+# df_yahoo = yf.download('AAPL', start='2000-01-01', end = '2010-01-01', progress = False)
 
-msft = yf.Ticker("MSFT")
-print(msft)
+# msft = yf.Ticker("MSFT")
 
-# get stock info
-print(msft.info)
+df = yf.download('AAPL',
+                 start='2000-01-01',
+                 end='2010-12-31',
+                 progress=False)
 
-# get historical market data
-hist = msft.history(period="max")
+df = df.loc[:, ['Adj Close']]
+print(df.index)
+df.rename(columns={'Adj Close': 'adj_close'}, inplace=True)
+print(df.adj_close)
+df['simple_rtn'] = df.adj_close.pct_change()
+# print(df)
+df['log_rtn'] = np.log(df.adj_close/df.adj_close.shift(1))
 
-# show actions (dividends, splits)
-msft.actions
 
-# show dividends
-msft.dividends
-
-# show splits
-msft.splits
+# df = pd.DataFrame([[2, 3], [3, 4], [5, 6]], index=[  'cobra', 'viper', 'side'], columns = ['max', 'rex'])
+# df = df.loc[:, 'max']
+# print(df)
