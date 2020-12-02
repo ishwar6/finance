da88177fa58f13eed7222138ba5c3292c09761cf
ishwar6 <iisudrj11@gmail.com>
2020-12-02 22:08:13 +0530

    Output

--- Basics/timejoin.py
+++ Basics/timejoin.py
@@ -10,7 +10,7 @@ trades = pd.DataFrame({
     'price': [51.95, 51.95, 720.77, 720.92, 98.00],
     'quantity': [75, 155, 100, 100, 100]},
     columns=['time', 'ticker', 'price', 'quantity'])
-
+print(trades)
 quotes = pd.DataFrame({
     'time': pd.to_datetime(['20160525 13:30:00.023',
                             '20160525 13:30:00.023',
@@ -25,7 +25,7 @@ quotes = pd.DataFrame({
     'ask': [720.93, 51.96, 51.98, 52.00, 720.93, 98.01, 720.88, 52.03]},
     columns=['time', 'ticker', 'bid', 'ask'])
 
-
+print(quotes)
 df_merge_asof = pd.merge_asof(trades, quotes,
                               on='time',
                               by='ticker')
@@ -39,3 +39,42 @@ df_merge_asof_tolerance = pd.merge_asof(trades, quotes,
                                         tolerance=pd.Timedelta('2ms'))
 
 print(df_merge_asof_tolerance)
+
+
+"""
+OUTPUT: 
+time ticker   price  quantity
+0 2016-05-25 13:30:00.023   MSFT   51.95        75
+1 2016-05-25 13:30:00.038   MSFT   51.95       155
+2 2016-05-25 13:30:00.048   GOOG  720.77       100
+3 2016-05-25 13:30:00.048   GOOG  720.92       100
+4 2016-05-25 13:30:00.048   AAPL   98.00       100
+
+
+                     time ticker     bid     ask
+0 2016-05-25 13:30:00.023   GOOG  720.50  720.93
+1 2016-05-25 13:30:00.023   MSFT   51.95   51.96
+2 2016-05-25 13:30:00.030   MSFT   51.97   51.98
+3 2016-05-25 13:30:00.041   MSFT   51.99   52.00
+4 2016-05-25 13:30:00.048   GOOG  720.50  720.93
+5 2016-05-25 13:30:00.049   AAPL   97.99   98.01
+6 2016-05-25 13:30:00.072   GOOG  720.50  720.88
+7 2016-05-25 13:30:00.075   MSFT   52.01   52.03
+
+
+                     time ticker   price  quantity     bid     ask
+0 2016-05-25 13:30:00.023   MSFT   51.95        75   51.95   51.96
+1 2016-05-25 13:30:00.038   MSFT   51.95       155   51.97   51.98
+2 2016-05-25 13:30:00.048   GOOG  720.77       100  720.50  720.93
+3 2016-05-25 13:30:00.048   GOOG  720.92       100  720.50  720.93
+4 2016-05-25 13:30:00.048   AAPL   98.00       100     NaN     NaN
+
+
+                     time ticker   price  quantity     bid     ask
+0 2016-05-25 13:30:00.023   MSFT   51.95        75   51.95   51.96
+1 2016-05-25 13:30:00.038   MSFT   51.95       155     NaN     NaN
+2 2016-05-25 13:30:00.048   GOOG  720.77       100  720.50  720.93
+3 2016-05-25 13:30:00.048   GOOG  720.92       100  720.50  720.93
+4 2016-05-25 13:30:00.048   AAPL   98.00       100     NaN     NaN
+
+"""
