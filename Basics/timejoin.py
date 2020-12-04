d7e141e6825d2435e6ce6bf7c6e411014df72743
ishwar6 <iisudrj11@gmail.com>
2020-12-04 18:33:49 +0530

    asfreq attribute

--- Basics/timejoin.py
+++ Basics/timejoin.py
@@ -1,4 +1,21 @@
 import pandas as pd
+index_values = (pd.date_range('1/1/2000',
+
+                              periods=17, freq='W'))
+
+# use M and W to see the effect of asfreq with fill_value argument
+
+series = (pd.Series([0.0, None, 2.0, None, 5, 6, 7, 4, 5, 6, 7, None, 4, 5, 6, 7, 10],
+                    index=index_values))
+df = pd.DataFrame({"Col_1": series})
+
+# to generate the data of month end only
+df1 = df.asfreq(freq='M', fill_value=9.0)
+# if something is already created: fill_value will not insert the value
+df2 = df.asfreq(freq='D', fill_value=9.0)
+df3 = df.asfreq(freq='W', fill_value=9.0)
+print(df2)
+
 
 trades = pd.DataFrame({
     'time': pd.to_datetime(['20160525 13:30:00.023',
@@ -10,7 +27,7 @@ trades = pd.DataFrame({
     'price': [51.95, 51.95, 720.77, 720.92, 98.00],
     'quantity': [75, 155, 100, 100, 100]},
     columns=['time', 'ticker', 'price', 'quantity'])
-print(trades)
+# print(trades)
 quotes = pd.DataFrame({
     'time': pd.to_datetime(['20160525 13:30:00.023',
                             '20160525 13:30:00.023',
@@ -25,12 +42,12 @@ quotes = pd.DataFrame({
     'ask': [720.93, 51.96, 51.98, 52.00, 720.93, 98.01, 720.88, 52.03]},
     columns=['time', 'ticker', 'bid', 'ask'])
 
-print(quotes)
+# print(quotes)
 df_merge_asof = pd.merge_asof(trades, quotes,
                               on='time',
                               by='ticker')
 
-print(df_merge_asof)
+# print(df_merge_asof)
 
 # sof merge within 2ms between the quote time and the trade time
 df_merge_asof_tolerance = pd.merge_asof(trades, quotes,
@@ -38,7 +55,7 @@ df_merge_asof_tolerance = pd.merge_asof(trades, quotes,
                                         by='ticker',
                                         tolerance=pd.Timedelta('2ms'))
 
-print(df_merge_asof_tolerance)
+# print(df_merge_asof_tolerance)
 
 
 """
