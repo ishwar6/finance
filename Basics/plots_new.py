544103a6c1d53f6eff5c5ad3abd08f295026c5c9
ishwar6 <iisudrj11@gmail.com>
2020-12-08 17:10:35 +0530

    Bars

--- Basics/plots_new.py
+++ Basics/plots_new.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 1]
+x = [1, 2, 3, 1]  # list
 y = [22, 3, 4, 33]
-plt.plot(x, y, label="First Line")
+# plt.plot(x, y, label="First Line")
 
-plt.plot([1, 2, 3, 53], [1, 2, 4, 5])
-plt.legend()  # to make the label appear we need this line
+# plt.plot([1, 2, 3, 53], [1, 2, 4, 5])
+plt.bar(x, y, label="runs")
+plt.legend()  # to make the label appear, we need this line
 plt.show()
