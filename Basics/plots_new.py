184f0fcb29211d949f90cc02f7670c879d12d263
ishwar6 <iisudrj11@gmail.com>
2020-12-08 17:21:04 +0530

    Histogram

--- Basics/plots_new.py
+++ Basics/plots_new.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 1]  # list
-y = [22, 3, 4, 33]
+x = [1, 2, 3, 32]  # list
+y = [21, 33, 44, 13]
 # plt.plot(x, y, label="First Line")
 
+
 # plt.plot([1, 2, 3, 53], [1, 2, 4, 5])
-plt.bar(x, y, label="runs")
+plt.bar(x, y, label="runs", color="r")
+plt.bar([12, 11, 41, 35], [53, 13, 25, 34], label="balls", color="b")
 plt.legend()  # to make the label appear, we need this line
 plt.show()
