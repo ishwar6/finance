e2aa6d5091a816fedb88dba10935abf7934e76b6
ishwar6 <iisudrj11@gmail.com>
2021-01-08 13:37:32 +0530

    Pdf

--- Basics/pre.py
+++ Basics/pre.py
@@ -1,20 +1,25 @@
 import PyPDF2
 
 # creating a pdf file object
-path = ''
-pdfFileObj = open('example.pdf', 'rb')
+path = '/Users/ishwarjangid/Desktop/CA for 2021 /Test Series/Test 1.pdf'
+pdfFileObj = open(path, 'rb')
 
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 # printing number of pages in pdf file
-print(pdfReader.numPages)
+a = pdfReader.numPages
 
 # creating a page object
 pageObj = pdfReader.getPage(0)
+print(type(pageObj))
 
 # extracting text from page
-print(pageObj.extractText())
+for i in range(a):
+    pageObj = pdfReader.getPage(0)
+
+
+# print(pageObj.extractText())
 
 # closing the pdf file object
 pdfFileObj.close()
